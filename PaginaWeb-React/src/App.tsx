91a27496fd1358a3424fe25db035fe10f9549118
Franco Balich <franco.balich@gmail.com>
2022-08-31 01:16:21 -0300

    add ButtonCallToAction

--- PaginaWeb-React/src/App.tsx
+++ PaginaWeb-React/src/App.tsx
@@ -1,11 +1,32 @@
 import { useState } from 'react'
 import './App.css'
+import ButtonCallToAction from './components/ButtonCallToAction'
 
 function App () {
   const [count, setCount] = useState(0)
 
   return (
     <div className='App'>
+      <ButtonCallToAction
+        text="Comunidad"
+        background={false}
+        style={1}
+      />
+      <ButtonCallToAction
+        text="Comunidad"
+        background={false}
+        style={1}
+      />
+      <ButtonCallToAction
+        text="Comunidad"
+        background={false}
+        style={1}
+      />
+      <ButtonCallToAction
+        text="Iniciar Sesión"
+        background={true}
+        style={1}
+      />
     </div>
   )
 }
--- PaginaWeb-React/src/components/ButtonCallToAction.tsx
+++ PaginaWeb-React/src/components/ButtonCallToAction.tsx
@@ -1,16 +1,24 @@
 import React from 'react'
 import "../styles/ButtonCallToAction.css"
+
 interface propButtonCallToAction{
-    text:string
+    text:string,
+    background:boolean,
+    style:Number
 }
 
-function ButtonCallToAction({text}:propButtonCallToAction) {
+function ButtonCallToAction({text,background,style}:propButtonCallToAction) {
   const clic=()=>{
     console.log("Se hizo clic")
   }
+  const selectorStyle=(backgroundIn:boolean, styleIn:Number)=>{
+    let styles
+    styles = (backgroundIn)?"withBackground ":"withoutBackground "
+    return styles + 'ButtonCallToAction'
+  }
 
   return (
-    <button onClick={clic} className='ButtonCallToAction'>
+    <button onClick={clic} className={selectorStyle(background,style)}>
       <span>{text}</span>
     </button>
   )
