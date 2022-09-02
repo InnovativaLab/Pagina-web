796d0d0a862ad0772289dcbcd661e9fa281d9594
Franco Balich <franco.balich@gmail.com>
2022-09-02 00:57:08 -0300

    Add fondo Olas

--- PaginaWeb-React/src/App.tsx
+++ PaginaWeb-React/src/App.tsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './components/Header'
+import MainInicio from './components/MainInicio'
+import fondoOlas from "./assets/fondoOlas.svg"
 function App () {
   const [count, setCount] = useState(0)
 
   return (
     <div className='App'>
       <Header />
+      <MainInicio />
+      <img src={fondoOlas} alt="Olas celestes"/>
     </div>
   )
 }
---
+++ PaginaWeb-React/src/components/MainInicio.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "../styles/MainInicio.css"
+function MainInicio() {
+  return (
+    <main className='mainInicio'>
+
+    </main>
+  )
+}
+
+export default MainInicio
