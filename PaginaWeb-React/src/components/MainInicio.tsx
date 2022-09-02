0cbf4640962a1db5e31525f76da63828229e6d09
Franco Balich <franco.balich@gmail.com>
2022-09-02 02:35:47 -0300

    Add content in MainInicio

--- PaginaWeb-React/src/components/MainInicio.tsx
+++ PaginaWeb-React/src/components/MainInicio.tsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import "../styles/MainInicio.css"
+import ItemMenu from './ItemMenu'
+import FotoRobot from "../assets/robotAzul.png"
+
 function MainInicio() {
   return (
     <main className='mainInicio'>
-
+      <div className='mainContainer'>
+        <div className='mainInformation'>
+          <h1>Aprende sobre tecnologia muy facilmente de forma colaborativa y online.</h1>
+          <h2>Cursos online sobre tecnologia al alcence de todos, con efoque practico</h2>
+          <ItemMenu
+              text="¡Empeza ahora!"
+              background={true}
+              style={1}
+            />
+        </div>
+        <img src={FotoRobot} alt="Foto de un robot azul"/>
+      </div>
     </main>
   )
 }
