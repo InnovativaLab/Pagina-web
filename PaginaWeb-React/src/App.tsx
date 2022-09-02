5c661d1e19392e1a6eeed36b9b2728d6f33844b5
Franco Balich <franco.balich@gmail.com>
2022-09-02 00:40:30 -0300

    Remplacer header

--- PaginaWeb-React/src/App.tsx
+++ PaginaWeb-React/src/App.tsx
@@ -1,36 +1,12 @@
 import { useState } from 'react'
 import './App.css'
-import ButtonCallToAction from './components/ButtonCallToAction'
-import Browser from './components/Browser'
-
+import Header from './components/Header'
 function App () {
   const [count, setCount] = useState(0)
 
   return (
     <div className='App'>
-      <div className='containerMenu'>
-        <Browser />
-        <ButtonCallToAction
-          text="Comunidad"
-          background={false}
-          style={2}
-        />
-        <ButtonCallToAction
-          text="Comunidad"
-          background={false}
-          style={0}
-        />
-        <ButtonCallToAction
-          text="Comunidad"
-          background={false}
-          style={0}
-        />
-        <ButtonCallToAction
-          text="Iniciar Sesión"
-          background={true}
-          style={1}
-        />
-      </div>
+      <Header />
     </div>
   )
 }
