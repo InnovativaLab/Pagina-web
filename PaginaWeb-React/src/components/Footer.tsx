3e877ed28402cead0af788aedeea9672f0d69a1f
Franco Balich <franco.balich@gmail.com>
2022-09-02 01:51:16 -0300

    Add Redes and fix styles

--- PaginaWeb-React/src/components/Footer.tsx
+++ PaginaWeb-React/src/components/Footer.tsx
@@ -1,38 +1,42 @@
 import React from 'react'
 import "../styles/Footer.css"
-
+import Redes from './Redes'
 function Footer() {
   return (
     <footer>
-        <div className='footerLinksContainer'>
-            <div className='footerLinksColumn'>
-                <p className='footerLinksTitle'>Column One</p>
-                <ul className='footerLinksList'>
-                    <li><a href='#'>Link</a></li>
-                    <li><a href='#'>Link</a></li>
-                    <li><a href='#'>Link</a></li>
-                    <li><a href='#'>Link</a></li>
-                </ul>
-            </div>
-            <div className='footerLinksColumn'>
-                <p className='footerLinksTitle'>Column One</p>
-                <ul className='footerLinksList'>
-                    <li><a href='#'>Link</a></li>
-                    <li><a href='#'>Link</a></li>
-                    <li><a href='#'>Link</a></li>
-                    <li><a href='#'>Link</a></li>
-                </ul>
-            </div>
-            <div className='footerLinksColumn'>
-                <p className='footerLinksTitle'>Column One</p>
-                <ul className='footerLinksList'>
-                    <li><a href='#'>Link</a></li>
-                    <li><a href='#'>Link</a></li>
-                    <li><a href='#'>Link</a></li>
-                    <li><a href='#'>Link</a></li>
-                </ul>
+        <div className='footerCallToAction'>
+            <Redes size='small'/>
+            <div className='footerLinksContainer'>
+                <div className='footerLinksColumn'>
+                    <p className='footerLinksTitle'>Column One</p>
+                    <ul className='footerLinksList'>
+                        <li><a href='#'>Link</a></li>
+                        <li><a href='#'>Link</a></li>
+                        <li><a href='#'>Link</a></li>
+                        <li><a href='#'>Link</a></li>
+                    </ul>
+                </div>
+                <div className='footerLinksColumn'>
+                    <p className='footerLinksTitle'>Column One</p>
+                    <ul className='footerLinksList'>
+                        <li><a href='#'>Link</a></li>
+                        <li><a href='#'>Link</a></li>
+                        <li><a href='#'>Link</a></li>
+                        <li><a href='#'>Link</a></li>
+                    </ul>
+                </div>
+                <div className='footerLinksColumn'>
+                    <p className='footerLinksTitle'>Column One</p>
+                    <ul className='footerLinksList'>
+                        <li><a href='#'>Link</a></li>
+                        <li><a href='#'>Link</a></li>
+                        <li><a href='#'>Link</a></li>
+                        <li><a href='#'>Link</a></li>
+                    </ul>
+                </div>
             </div>
         </div>
+        
     </footer>
   )
 }
