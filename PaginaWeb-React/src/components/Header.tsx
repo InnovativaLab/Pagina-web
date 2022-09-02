7c4fc93725e9fc4f0e4b4c9e582aaa0c1eb1f0f8
Franco Balich <franco.balich@gmail.com>
2022-09-02 00:40:14 -0300

    Create Header component

---
+++ PaginaWeb-React/src/components/Header.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import ItemMenu from './ItemMenu'
+import Browser from './Browser'
+import LogoBanner from './LogoBanner'
+
+function Header() {
+  return (
+    <header>
+        <div className='containerMenu'>
+      <LogoBanner />
+      <Browser />
+        <ItemMenu
+          text="Cursos"
+          background={false}
+          style={2}
+        />
+        <ItemMenu
+          text="Comunidad"
+          background={false}
+          style={0}
+        />
+        <ItemMenu
+          text="Sobre nosotros"
+          background={false}
+          style={0}
+        />
+        <ItemMenu
+          text="Contacto"
+          background={false}
+          style={0}
+        />
+        <ItemMenu
+          text="Iniciar Sesión"
+          background={true}
+          style={1}
+        />
+      </div>
+    </header>
+  )
+}
+
+export default Header
