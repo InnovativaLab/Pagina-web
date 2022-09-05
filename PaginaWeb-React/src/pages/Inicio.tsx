71b6579d52fb44d9eab9bbace4d142fc112a53c8
Franco Balich <franco.balich@gmail.com>
2022-09-05 17:00:08 -0300

    Add AboutUs section

--- PaginaWeb-React/src/pages/Inicio.tsx
+++ PaginaWeb-React/src/pages/Inicio.tsx
@@ -2,14 +2,20 @@ import React from 'react'
 import MainInicio from '../components/MainInicio'
 import fondoOlas from '../assets/fondoOlas.svg'
 import './styles/Inicio.css'
+import learnImg from "../assets/svgLearn.svg"
 
 function Inicio () {
   return (
-    <div>
+    <div className='presentation'>
       <MainInicio />
-      <div className='contenedorFondoOlas'>
-        <img className='FondoOlas' src={fondoOlas} alt='Olas celestes' />
-      </div>
+      <section className='weObjects'>
+        <img src={learnImg} alt='Ilustracion aprendiendo' />
+        <div className='weObjectsinfo'>
+          <p className='weObjectsTitle'>Nuestro objetivo</p>
+          <p className='weObjectsText'>Somos una plataforma educativa con el objetivo de ser la plataforma educativa que propicie la transformación social mediante la promoción e impulso de la tecnología y sus pilares. <br /><br />Nos encargamos de garantizar una educación de calidad proporcionando una formación en base tecnológica mediante la utilización de herramientas interactivas con el fin de capacitar a los alumnos en la creación de proyectos autónomos de triple impacto.</p>
+        </div>
+      </section>
+      <img className='FondoOlas' src={fondoOlas} alt='Olas celestes' />
     </div>
   )
 }
