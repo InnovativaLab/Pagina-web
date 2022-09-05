83894d3f6e95dc96a3268d93e7dfec0db512ce5b
Franco Balich <franco.balich@gmail.com>
2022-09-05 16:26:31 -0300

    Add structure in login

--- PaginaWeb-React/src/pages/Login.tsx
+++ PaginaWeb-React/src/pages/Login.tsx
@@ -1,6 +1,36 @@
+import "./styles/login.css"
+import olas from "../assets/OlaLogin.svg"
+import Redes from '../components/Redes';
+import Title from '../components/Title';
+import Subtitle from '../components/Subtitle';
+import ItemMenu from "../components/ItemMenu";
+import TextBox from "../components/TextBox";
+import {Link} from "react-router-dom"
 function Login() {
   return (
     <div>
+      <main className='mainLogin'>
+        <Redes size="big"/>
+        <form  className='loginForm'>
+          <Title msg="Inicie sesión"/>
+          <Subtitle msg="Email" />
+          <TextBox placeholder="Ingresa tu correo electrónico" />
+          <Subtitle msg="Contraseña" />
+          <TextBox placeholder="Ingresa tu contraseña" />
+          <Link className="secondaryButton" to="/recoverpws" >
+              ¿Olvidaste tu contraseña?
+          </Link>
+          <ItemMenu
+            text='Iniciar sesion'
+            background
+            style={3}
+          />
+          <Link className="secondaryButton fullSpace" to="/signin" >
+            Registrarse
+          </Link>
+        </form>
+        <img className="olasLogin" src={olas} alt="Olas azules de decoracion" />
+      </main>
     </div>
   )
 }
