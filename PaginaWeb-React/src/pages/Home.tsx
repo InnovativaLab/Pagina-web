bb45199e5e12b0b59ba5a7ff591830ec8d803d54
Franco Balich <franco.balich@gmail.com>
2022-09-05 17:12:20 -0300

    Add structure in Home page

--- PaginaWeb-React/src/pages/Home.tsx
+++ PaginaWeb-React/src/pages/Home.tsx
@@ -1,6 +1,11 @@
+import Title from '../components/Title';
+import "./styles/Home.css"
 function Home () {
   return (
-    <div />
+    <div className='home'>
+      <p className='msgAlert'>¡Hola, Franco!</p>
+      <p className='homeSubtitulo'>Tus cursos</p>
+    </div>
   )
 }
 export default Home
