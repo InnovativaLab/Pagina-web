1d72f30d50d2564ec0ca022f1838ef9c0069cd42
Franco Balich <franco.balich@gmail.com>
2022-09-07 01:42:17 -0300

    Complete responsive menu

--- PaginaWeb-React/src/components/Header.tsx
+++ PaginaWeb-React/src/components/Header.tsx
@@ -2,12 +2,30 @@ import React from 'react'
 import ItemMenu from './ItemMenu'
 import Browser from './Browser'
 import LogoBanner from './LogoBanner'
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
+import "../styles/Header.css"
+import burger from "../assets/bx-menu.svg"
 
 function Header () {
+  const   btnMenu = document.getElementById("openMenuButtom")
+  const   btnMenuCerrar = document.getElementById("closeMenuButtom")
+  const   menu = document.getElementById("menu")
+
+  const[state,setState]= useState(" hide");
+
+  const cambiarEstadoMenu = ()=>{
+    if(state==="show"){
+      setState("hide")
+    }
+    else{
+      setState("show")
+    }
+  }
   return (
-    <header>
-      <div className='containerMenu'>
+    <header >
+      <button onClick={cambiarEstadoMenu} className={`openMenuButtom button${state}`} id='openMenuButtom'><img src={burger} alt="Icono menu"  /></button>
+      <div className={`containerMenu menu${state}`}>
         <LogoBanner />
         <Browser />
         <Link to='/' className='buttonItemMenu simple'>
@@ -25,6 +43,7 @@ function Header () {
         <Link to='/login' className='buttonItemMenu withBackground ItemMenu'>
           <span>Iniciar Sesión</span>
         </Link>
+        <button  onClick={cambiarEstadoMenu} className={`buttonItemMenu simple closeMenuButtom`}  id='closeMenuButtom'>Cerrar</button>
       </div>
     </header>
   )
