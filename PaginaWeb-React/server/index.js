88a73fd75196d88d4924c7e3a55644ab3f049b53
Franco Balich <franco.balich@gmail.com>
2022-09-19 01:46:53 -0300

    Add run all

---
+++ PaginaWeb-React/server/index.js
@@ -0,0 +1,16 @@
+import pino from 'express-pino-logger'
+import bodyParser from 'body-parser'
+import * as dotenv from 'dotenv'
+import express from'express'
+
+const app = express()
+const PORT = 3001
+dotenv.config()
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(pino())
+
+
+app.listen(PORT, () =>
+  console.log(`Es servidor se esta ejecutando en el puerto:${PORT}`)
+)
