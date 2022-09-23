ab81bac64c6270f4f4fb018d5b7a9c2686f4c532
Franco Balich <franco.balich@gmail.com>
2022-09-23 01:24:30 -0300

    Add link to course

--- PaginaWeb-React/src/pages/Busqueda.tsx
+++ PaginaWeb-React/src/pages/Busqueda.tsx
@@ -1,9 +1,11 @@
 import './styles/Busqueda.css'
+import { Link } from 'react-router-dom'
 
 function Busqueda () {
   return (
     <div className='busqueda'>
       <h1 style={{"color":"#fff"}}>Busqueda</h1>
+      <Link to='/course'>Ir al curso</Link>
     </div>
   )
 }
