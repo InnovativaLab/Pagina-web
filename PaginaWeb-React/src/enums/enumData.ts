51f415d915ccb0c0f6a5bd653b3588f2b34bfa90
Franco Balich <franco.balich@gmail.com>
2022-09-28 00:49:35 -0300

    Add enum to types of courses

---
+++ PaginaWeb-React/src/enums/enumData.ts
@@ -0,0 +1,9 @@
+export enum enumCourseType {
+    Robotica = "Robotica",
+    Programacion = "Programacion",
+    DesarrolloWeb = "DesarrolloWeb",
+    Electronica = "Electronica",
+    Arduino = "Arduino",
+    Tinkercad ="Tinkercad",
+    Impresion3D = "Impresion3D"
+}
