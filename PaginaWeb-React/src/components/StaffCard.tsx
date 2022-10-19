05b81017e39997c1282e65897a375ad5f03686db
Franco Balich <franco.balich@gmail.com>
2022-10-19 00:40:20 -0300

    Add StaffCard component

---
+++ PaginaWeb-React/src/components/StaffCard.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import fotoFranco  from '../assets/staff/francobalich.png'
+import '../styles/StaffCard.css'
+
+interface propStaffCard{
+    name:string,
+    description:string
+}
+
+function StaffCard({name,description}:propStaffCard) {
+  return (
+    <div className='staffCard'>
+        <img src={fotoFranco} alt={`Foto de ${name}`} />
+        <p className='staffCardTitle'>{name}</p>
+        <p className='staffCardText'>{description}</p>
+    </div>
+  )
+}
+
+export default StaffCard
