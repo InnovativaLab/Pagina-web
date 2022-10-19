5a16db916913d44b72a31e7976b289ca54ba0bd2
Franco Balich <franco.balich@gmail.com>
2022-10-19 00:13:43 -0300

    Add call to apis

--- PaginaWeb-React/server/index.js
+++ PaginaWeb-React/server/index.js
@@ -10,6 +10,30 @@ dotenv.config()
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(pino())
 
+
+
+app.get('/api', async (req, res, next) => {
+  try {res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    let validUser="franco"
+    let course = {
+      title:"Curso Arduino",
+      tags:["Arduino","Tinkercad","Robotica"],
+      text:"En este cursos aprenderas los conceptos basicos de Arduino, electronica basica y sobre simuladores de robotica con el objetivo de que al finalizar el mismo seas capaz de crear tu propio robot."
+    }
+    const user = req.query.user
+    if(user===validUser){
+      res.send(course)
+    }
+    else{
+      res.status(401).send('El usuario no tiene ese curso asignado')
+    }
+  } catch (err) {
+    console.log(err)
+    res.status(401).send('Hubo un error al reproducir el mensaje.')
+  }
+})
+
 app.listen(PORT, () =>
   console.log(`Es servidor se esta ejecutando en el puerto:${PORT}`)
 )
--- PaginaWeb-React/src/pages/Home.tsx
+++ PaginaWeb-React/src/pages/Home.tsx
@@ -2,19 +2,21 @@ import Title from '../components/Title'
 import './styles/Home.css'
 import Card from '../components/Card'
 import { enumCourseType } from '../enums/enumData'
+import axios from 'axios'
+
 function Home () {
+  let respuesta =""
+  /*const readCourses=async(user:string)=>{
+    const res = await axios.get(`http://localhost:3001/api?user=franco`)
+    console.log(res.data);
+    respuesta = res.data
+    return res.data
+  }*/
   return (
     <div className='home'>
       <p className='msgAlert'>¡Hola, Franco!</p>
       <p className='homeSubtitulo'>Tus cursos</p>
       <div className='cardsContinaer'>
-        <Card
-          img=''
-          alt='robot'
-          title='Introduccion a Arduino'
-          text='En este cursos aprenderas los conceptos basicos de Arduino, electronica basica y sobre simuladores de robotica con el objetivo de que al finalizar el mismo seas capaz de crear tu propio robot.'
-          tags={[enumCourseType.Arduino, enumCourseType.Tinkercad, enumCourseType.Robotica]}
-        />
         <Card
           img=''
           alt='robot'
