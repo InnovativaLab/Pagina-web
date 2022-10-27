b887e3f3a090b6d1270e53aa2b35daffbd53ff55
Franco Balich <franco.balich@gmail.com>
2022-10-26 22:35:55 -0300

    Add res 404

--- PaginaWeb-React/server/index.js
+++ PaginaWeb-React/server/index.js
@@ -1,20 +1,23 @@
 import pino from 'express-pino-logger'
 import bodyParser from 'body-parser'
-import * as dotenv from 'dotenv'
 import express from 'express'
 import userRouter from './routes/user.routes.js'
 import coursesRouter from './routes/courses.routes.js'
-
+import {PORT} from './config.js'
 const app = express()
-const PORT = 3001
-dotenv.config()
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(pino())
+app.use(express.json())
 
 app.use('/api', userRouter)
 app.use('/api', coursesRouter)
 
+app.use((req,res,next)=>{
+  res.status(404).json({
+      message:'Endpoint not found'
+  })
+})
 app.listen(PORT, () =>
   console.log(`Es servidor se esta ejecutando en el puerto:${PORT}`)
 )
