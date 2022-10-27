387b3dc4fabf8ff2db55d6349696d91fb870f6cb
Franco Balich <franco.balich@gmail.com>
2022-10-26 21:43:42 -0300

    Add config.js

---
+++ PaginaWeb-React/server/config.js
@@ -0,0 +1,10 @@
+import {config} from 'dotenv'
+config()
+console.log(process.env.PORT);
+
+export const PORT = process.env.PORT || 3000
+export const DB_HOST = process.env.DB_HOST || 'localhost'
+export const DB_PORT = process.env.DB_PORT || '3306'
+export const DB_USER = process.env.DB_USER || 'root'
+export const DB_PASSWORD = process.env.DB_PASSWORD || '1234'
+export const DB_DATABASE = process.env.DB_DATABASE || 'companydb'
