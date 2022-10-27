1bd0fe7e1ec5947429e2416079b175fe2a83ade1
Franco Balich <franco.balich@gmail.com>
2022-10-26 22:35:41 -0300

    Read env

--- PaginaWeb-React/server/config.js
+++ PaginaWeb-React/server/config.js
@@ -2,7 +2,7 @@ import {config} from 'dotenv'
 config()
 console.log(process.env.PORT);
 
-export const PORT = process.env.PORT || 3000
+export const PORT = process.env.PORT || '3000'
 export const DB_HOST = process.env.DB_HOST || 'localhost'
 export const DB_PORT = process.env.DB_PORT || '3306'
 export const DB_USER = process.env.DB_USER || 'root'
