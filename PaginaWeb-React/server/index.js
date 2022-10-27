d5b8ba250470a0ecd11134b92e9a524286b65267
Franco Balich <franco.balich@gmail.com>
2022-10-27 17:23:43 -0300

    Add headers

--- PaginaWeb-React/server/index.js
+++ PaginaWeb-React/server/index.js
@@ -9,9 +9,18 @@ const app = express()
 app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(pino())
+// Configurar cabeceras y cors
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
 
 app.use('/api', userRouter)
 app.use('/api', coursesRouter)
+
 
 app.use((req, res, next) => {
   res.status(404).json({
