e80ceb8f5e5deeaa113c811e5ea55be6a91282cc
Franco Balich <franco.balich@gmail.com>
2022-10-28 14:17:20 -0300

    Add dinamic courses

--- PaginaWeb-React/src/pages/Home.tsx
+++ PaginaWeb-React/src/pages/Home.tsx
@@ -2,63 +2,48 @@ import Title from '../components/Title'
 import './styles/Home.css'
 import Card from '../components/Card'
 import { enumCourseType } from '../enums/enumData'
-import axios from 'axios'
+import axios from 'axios';
+const API_LINK='http://localhost:3001'
+import { Course } from '../types';
+import { useState,useEffect } from 'react';
 
 function Home () {
-  const respuesta = ''
-  /* const readCourses=async(user:string)=>{
-    const res = await axios.get(`http://localhost:3001/api?user=franco`)
-    console.log(res.data);
-    respuesta = res.data
-    return res.data
-  } */
+  let coursesCards:JSX.Element[]=[]
+  const [listCourses,setListCourses]= useState(coursesCards)
+  const getCourses=async()=>{
+    console.log("Cargando Cursos...");
+    try {
+      const res = await axios.get(`${API_LINK}/api/courses`)
+      const courses:Course[] = res.data
+      let coursesElements:JSX.Element[]=[]
+      coursesElements =  Object.values(courses).map(course=>
+      <Card
+        img=''
+        alt='robot'
+        title={course.Titulo}
+        text={course.Descripcion}
+        tags={[enumCourseType.Arduino, enumCourseType.Tinkercad, enumCourseType.Robotica]}
+      />)
+      return coursesElements
+    } catch (err:any) {
+      console.log(err.response)
+      return coursesCards
+    }
+  }
+  useEffect(() => {
+    getCourses().then((listaCursos)=>{
+      setListCourses(listaCursos)
+      console.log(coursesCards)
+    })
+  },[]);
   return (
     <div className='home'>
       <p className='msgAlert'>¡Hola, Franco!</p>
       <p className='homeSubtitulo'>Tus cursos</p>
       <div className='cardsContinaer'>
-        <Card
-          img=''
-          alt='robot'
-          title='Introduccion a Arduino'
-          text='En este cursos aprenderas los conceptos basicos de Arduino, electronica basica y sobre simuladores de robotica con el objetivo de que al finalizar el mismo seas capaz de crear tu propio robot.'
-          tags={[enumCourseType.Arduino, enumCourseType.Tinkercad, enumCourseType.Robotica]}
-        />
-        <Card
-          img=''
-          alt='robot'
-          title='Introduccion a Arduino'
-          text='En este cursos aprenderas los conceptos basicos de Arduino, electronica basica y sobre simuladores de robotica con el objetivo de que al finalizar el mismo seas capaz de crear tu propio robot.'
-          tags={[enumCourseType.Arduino, enumCourseType.Tinkercad, enumCourseType.Robotica]}
-        />
-        <Card
-          img=''
-          alt='robot'
-          title='Introduccion a Arduino'
-          text='En este cursos aprenderas los conceptos basicos de Arduino, electronica basica y sobre simuladores de robotica con el objetivo de que al finalizar el mismo seas capaz de crear tu propio robot.'
-          tags={[enumCourseType.Arduino, enumCourseType.Tinkercad, enumCourseType.Robotica]}
-        />
-        <Card
-          img=''
-          alt='robot'
-          title='Introduccion a Arduino'
-          text='En este cursos aprenderas los conceptos basicos de Arduino, electronica basica y sobre simuladores de robotica con el objetivo de que al finalizar el mismo seas capaz de crear tu propio robot.'
-          tags={[enumCourseType.Arduino, enumCourseType.Tinkercad, enumCourseType.Robotica]}
-        />
-        <Card
-          img=''
-          alt='robot'
-          title='Introduccion a Arduino'
-          text='En este cursos aprenderas los conceptos basicos de Arduino, electronica basica y sobre simuladores de robotica con el objetivo de que al finalizar el mismo seas capaz de crear tu propio robot.'
-          tags={[enumCourseType.Arduino, enumCourseType.Tinkercad, enumCourseType.Robotica]}
-        />
-        <Card
-          img=''
-          alt='robot'
-          title='Introduccion a Arduino'
-          text='En este cursos aprenderas los conceptos basicos de Arduino, electronica basica y sobre simuladores de robotica con el objetivo de que al finalizar el mismo seas capaz de crear tu propio robot.'
-          tags={[enumCourseType.Arduino, enumCourseType.Tinkercad, enumCourseType.Robotica]}
-        />
+        {
+          listCourses
+        }
       </div>
     </div>
   )
