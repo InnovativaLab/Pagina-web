741e4584c45fabd8d898eade80ac9e680313bd1c
Franco Balich <franco.balich@gmail.com>
2022-10-28 14:17:36 -0300

    Add user and course type

---
+++ PaginaWeb-React/src/types.ts
@@ -0,0 +1,28 @@
+export type Course={
+    Id:number,
+    Titulo:string,
+    Subtitulo:string,
+    Nivel:string,
+    Categoria:string,
+    Subcategoria:string,
+    Descripcion:string,
+    Estado:string,
+    Idioma:string,
+    TiempoDePublicacion:string,
+    PrecioEnPesos:string,
+    PrecioEnDolares:string,
+    ImagenDePortada:string,
+    VideoPromocional:string,
+    MensajeDeBienvenida:string,
+    MensajeDeFelicitaciones:string
+}
+export type User={
+    Nombre:string,
+    Apellido:string,
+    Genero:string,
+    Contraseña:string,
+    Email:string,
+    NombreDeUsuario:string,
+    Preferencias:string,
+    Estado:string
+}
