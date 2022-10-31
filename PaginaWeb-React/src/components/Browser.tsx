e21b4fde725652d3f78d31bcfee146ec36c1c545
Franco Balich <franco.balich@gmail.com>
2022-10-31 20:04:45 -0300

    Add cards to find

--- PaginaWeb-React/src/components/Browser.tsx
+++ PaginaWeb-React/src/components/Browser.tsx
@@ -1,23 +1,25 @@
+import { useNavigate } from 'react-router-dom'
 import lupa from '../assets/lupa.svg'
-import '../styles/Browser.css'
-import { Link } from 'react-router-dom'
 import { useState } from 'react'
+import '../styles/Browser.css'
 
 interface propBrowser {
   closeFunction: any
 }
 function Browser ({ closeFunction }: propBrowser) {
-  const [browse, setbrowse] = useState('')
+  const [browse, setbrowse] = useState('a')
   const readBrowse = (event:any)=>{
     setbrowse(event.target.value)
+  }
+  const navigate = useNavigate()
+  const goToBrowse=()=>{
+    navigate(`/browse/${browse}`)
   }
   return (
     <div className='browserContainer'>
       <input type='text' placeholder='Buscar curso' onChange={readBrowse}/>
-      <button onClick={closeFunction}>
-        <Link to={`/browse/${browse}`}>
+      <button onClick={goToBrowse}>
           <img src={lupa} alt='' />
-        </Link>
       </button>
     </div>
   )
--- PaginaWeb-React/src/pages/Busqueda.tsx
+++ PaginaWeb-React/src/pages/Busqueda.tsx
@@ -1,12 +1,11 @@
-import './styles/Busqueda.css'
-import Card from '../components/Card'
-import { enumCategoriaCurso } from '../enum'
-import Tag from '../components/Tag'
-import { Route, Routes, Link, useParams, Outlet, NavLink } from 'react-router-dom'
-import axios from 'axios'
-import { useState ,useEffect} from 'react';
 import { loadCards } from '../services/cards'
+import { enumCategoriaCurso } from '../enum'
+import { useParams} from 'react-router-dom'
+import { useState ,useEffect} from 'react'
+import Tag from '../components/Tag'
 import { Course } from '../types'
+import './styles/Busqueda.css'
+import axios from 'axios'
 
 const API_LINK = 'http://localhost:3001'
 
@@ -14,10 +13,9 @@ function Busqueda () {
   const coursesCards: Course[] = []
   const { toFind } = useParams()
   const [num,setNum] =useState(0)
-  const [listCourses,setListCourses] =useState(coursesCards)
+  const [listCourses,setListCourses]=useState(coursesCards)
   const sendDataToFind = async () => {
     const res = await axios.post(`${API_LINK}/api/courses/get`, { "Titulo": toFind })
-   // setNum(res.data)
     return res.data
   }
   useEffect(() => {
@@ -26,9 +24,8 @@ function Busqueda () {
       console.log(listaCursos)
       setListCourses(listaCursos)
     })
-  }, [])
-  //let info = sendDataToFind((toFind!==undefined)?toFind:"")
- // console.log(info)
+  }, [toFind])
+
   return (
     <div className='browse'>
       <p className='browseTitle'>{`${num} resultados encontrados para "${toFind}"`}</p>
@@ -39,9 +36,8 @@ function Busqueda () {
         <Tag type={enumCategoriaCurso.Impresion3D} />
       </div>
       <div className='cardsContinaer'>
-        {loadCards(listCourses)}
+        { loadCards(listCourses) }
       </div>
-      <Link to='/course'>Ir al curso</Link>
     </div>
   )
 }
