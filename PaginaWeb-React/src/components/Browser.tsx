a745152e56a91a4baf0cf65ad2dd004706d9469c
Franco Balich <franco.balich@gmail.com>
2022-10-31 19:22:30 -0300

    Read data

--- PaginaWeb-React/src/components/Browser.tsx
+++ PaginaWeb-React/src/components/Browser.tsx
@@ -1,15 +1,19 @@
-import React from 'react'
 import lupa from '../assets/lupa.svg'
 import '../styles/Browser.css'
 import { Link } from 'react-router-dom'
+import { useState, MouseEvent, ChangeEvent } from 'react'
 
 interface propBrowser {
   closeFunction: any
 }
 function Browser ({ closeFunction }: propBrowser) {
+  const [browse, setbrowse] = useState('')
+  const readBrowse = (event:any)=>{
+    setbrowse(event.target.value)
+  }
   return (
     <div className='browserContainer'>
-      <input type='text' placeholder='Buscar curso' />
+      <input type='text' placeholder='Buscar curso' onChange={readBrowse}/>
       <button onClick={closeFunction}>
         <Link to='/browse'>
           <img src={lupa} alt='' />
