4508f8c9477fa1cd732d5df987f070f990787c24
Franco Balich <franco.balich@gmail.com>
2022-11-01 19:20:26 -0300

    Upgrade code

--- PaginaWeb-React/src/components/TextBox.tsx
+++ PaginaWeb-React/src/components/TextBox.tsx
@@ -6,17 +6,19 @@ interface propTextBox {
   textType?: string
 }
 function TextBox ({ placeholder, getData, textType }: propTextBox) {
+  const saveData = (event:any) => {
+    if (getData !== undefined) {
+      getData(event.target.value)
+    } else {
+      console.log('hizo un clic')
+    }
+  }
   return (
     <input
-      className='textbox' onChange={
-      (event) => {
-        if (getData !== undefined) {
-          getData(event.target.value)
-        } else {
-          console.log('hizo un clic')
-        }
-      }
-} type={(textType !== undefined) ? textType : 'text'} placeholder={placeholder}
+      className='textbox'
+      onChange={saveData}
+      type={(textType !== undefined) ? textType : 'text'}
+      placeholder={placeholder}
     />
   )
 }
