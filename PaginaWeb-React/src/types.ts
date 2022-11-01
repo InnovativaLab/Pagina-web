8489bd1b8c6c587f1e19f10ede695be61fbeb3aa
Franco Balich <franco.balich@gmail.com>
2022-11-01 14:41:55 -0300

    Modify name of user type

--- PaginaWeb-React/src/types.ts
+++ PaginaWeb-React/src/types.ts
@@ -16,7 +16,7 @@ export interface Course {
   MensajeDeBienvenida: string
   MensajeDeFelicitaciones: string
 }
-export interface User {
+export interface Usuario {
   Nombre: string
   Apellido: string
   Genero: string
