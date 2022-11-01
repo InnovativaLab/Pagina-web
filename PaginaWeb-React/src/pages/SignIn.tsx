ef3b2cac921695e06e96c21cb54bd5403e187e41
Franco Balich <franco.balich@gmail.com>
2022-11-01 20:01:33 -0300

    Add useStates and eventHandlers

--- PaginaWeb-React/src/pages/SignIn.tsx
+++ PaginaWeb-React/src/pages/SignIn.tsx
@@ -5,12 +5,40 @@ import Subtitle from '../components/Subtitle'
 import TextBox from '../components/TextBox'
 import Redes from '../components/Redes'
 import Title from '../components/Title'
-import { useEffect } from 'react'
+import { useEffect, useState, MouseEvent } from 'react'
 import './styles/login.css'
+import { Usuario } from '../types';
+import { sendDataSignIn } from '../services/services';
+import { enumGenero } from '../enum';
 
 function SignIn () {
   const sesion = userSesion.getInstance()
   const navigate = useNavigate()
+  let user: Usuario|undefined
+
+  const [NombreDeUsuario,setNombreDeUsuario] = useState('')
+  const [ContraseñaConfirmacion,setContraseñaConfirmacion] = useState('')
+  const [Contraseña,setContraseña] = useState('')
+  const [Apellido,setApellido] = useState('')
+  const [Nombre,setNombre] = useState('')
+  const [Email,setEmail] = useState('')
+  const [Genero,setGenero] = useState('')
+
+  const signIn = (e: MouseEvent<HTMLButtonElement | HTMLAnchorElement>): void => {
+    try {
+      e.preventDefault()
+      user = {NombreDeUsuario,Nombre,Apellido,Contraseña,Email, Preferencias:"", Estado:"Sin verificar", Genero}
+      sendDataSignIn(user).then((data) => {
+        sesion.saveSesion(data)
+        navigate('/home', { replace: true })
+      })
+    } catch (err: any) {
+      console.log(err.response)
+    }
+  }
+  const saveGender = (event:any) => {
+      setGenero(event.target.value)
+  }
   useEffect(() => {
     if (sesion.isLogged()) {
       navigate('/home', { replace: true })
@@ -23,25 +51,37 @@ function SignIn () {
         <form className='loginForm'>
           <Title msg='Registrarse' />
 
-          <Subtitle msg='Nombre y Apellido' />
-          <TextBox placeholder='Ingresa tu nombre y Apellido' />
+          <Subtitle msg='Nombre' />
+          <TextBox placeholder='Ingresa tu Nombre' getData={(value: any) => setNombre(value)} />
+
+          <Subtitle msg='Apellido' />
+          <TextBox placeholder='Ingresa tu Apellido' getData={(value: any) => setApellido(value)} />
 
           <Subtitle msg='Nombre de usuario' />
-          <TextBox placeholder='Ingresa tu nombre de usuario' />
+          <TextBox placeholder='Ingresa tu nombre de usuario' getData={(value: any) => setNombreDeUsuario(value)} />
+
+          <Subtitle msg='Genero' />
+          <select name="select" className='combobox' onChange={saveGender}>
+            <option value={enumGenero.PrefieroNoDecirlo}>{enumGenero.PrefieroNoDecirlo}</option>
+            <option value={enumGenero.Masculino}>{enumGenero.Masculino}</option>
+            <option value={enumGenero.Femenino}>{enumGenero.Femenino}</option>
+            <option value={enumGenero.Otro}>{enumGenero.Otro}</option>
+          </select>
 
           <Subtitle msg='Email' />
-          <TextBox placeholder='Ingresa tu correo electrónico' />
+          <TextBox placeholder='Ingresa tu correo electrónico' getData={(value: any) => setEmail(value)}  textType="email"/>
 
           <Subtitle msg='Contraseña' />
-          <TextBox placeholder='Ingresa tu contraseña' />
+          <TextBox placeholder='Ingresa tu contraseña' getData={(value: any) => setContraseña(value)} textType="password"/>
 
           <Subtitle msg='Confirmar contraseña' />
-          <TextBox placeholder='Confirme tu contraseña' />
+          <TextBox placeholder='Confirme tu contraseña' getData={(value: any) => setContraseñaConfirmacion(value)} textType="password"/>
           <Link to='/' className='fullSpace'>
             <ItemMenu
               text='Crear cuenta'
               background
               style={3}
+              onClick={signIn}
             />
           </Link>
           <Link className='secondaryButton fullSpace' to='/'>
