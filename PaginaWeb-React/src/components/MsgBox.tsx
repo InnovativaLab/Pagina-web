01d2035675ab8337d955011d68e916dd10d80c20
Franco Balich <franco.balich@gmail.com>
2022-11-02 01:05:32 -0300

    Create component MsgBox

---
+++ PaginaWeb-React/src/components/MsgBox.tsx
@@ -0,0 +1,27 @@
+import '../styles/MsgBox.css'
+import { useState,useEffect } from 'react';
+
+interface propMsgBox{
+    text:string
+}
+
+function MsgBox({text}:propMsgBox) {
+  const [msg,setMsg]=useState(<></>)
+  
+  const generateMessage =()=>{
+      if(text!==''){
+          return <div className='msgBox'>{text}</div>
+      }
+      return <></>
+  }
+  useEffect(() => {
+    setMsg(generateMessage())
+  }, [])
+  return (
+  <>
+    {msg}
+  </>
+  )
+}
+
+export default MsgBox
