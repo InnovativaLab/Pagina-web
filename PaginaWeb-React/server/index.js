18cfae4b94db267c88392e66bb9ec209484aab5b
Franco Balich <franco.balich@gmail.com>
2022-11-02 15:08:48 -0300

    Add compresion text

--- PaginaWeb-React/server/index.js
+++ PaginaWeb-React/server/index.js
@@ -1,17 +1,20 @@
 import pino from 'express-pino-logger'
 import bodyParser from 'body-parser'
 import express from 'express'
+import compression  from 'compression'
 import userRouter from './routes/user.routes.js'
 import coursesRouter from './routes/courses.routes.js'
 import { PORT } from './config.js'
-const app = express()
 
+const app = express()
+app.use(compression());
 app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(pino())
 // Configurar cabeceras y cors
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*')
+  //res.header('Accept-Encoding: gzip, compress, br')
   res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method')
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE')
   res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE')
