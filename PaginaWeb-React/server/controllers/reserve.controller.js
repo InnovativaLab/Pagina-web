d5be930b2de9800724c173c50a7ef535048ab1af
Franco Balich <franco.balich@gmail.com>
2022-11-03 18:09:21 -0300

    Add ABM reserve

---
+++ PaginaWeb-React/server/controllers/reserve.controller.js
@@ -0,0 +1,46 @@
+import { pool } from '../db.js'
+
+export const getReserve = async (req, res) => {
+  try {
+    const { NombreDeUsuario } = req.body
+    if (NombreDeUsuario !== undefined) {
+      const [rows] = await pool.query('SELECT curso.Id, curso.Titulo, curso.Subtitulo, curso.Nivel, curso.Categoria, curso.Subcategoria, curso.Descripcion, curso.Estado, curso.Idioma, curso.TiempoDePublicacion, curso.PrecioEnPesos, curso.PrecioEnDolares, curso.ImagenDePortada, curso.MensajeDeBienvenida, curso.MensajeDeFelicitaciones FROM coursesdb.cursousuario INNER JOIN curso On cursousuario.CursoID= curso.Id where cursousuario.NombreDeUsuario =?;',[NombreDeUsuario])
+      return res.json(rows)
+    }
+     else {
+      return res.status(500).json({
+        message: 'Data incorrect'
+      })
+    }
+  } catch (error) {
+    console.log(error)
+    return res.status(500).json({
+      message: 'Something goes wrong'
+    })
+  }
+}
+
+export const createReserve = async (req, res) => {
+  try {
+    const { CursoId, NombreDeUsuario} = req.body
+    const [rows] = await pool.query('INSERT INTO coursesdb.cursousuario (CursoID,NombreDeUsuario) VALUES (?,?);', [CursoId, NombreDeUsuario])
+    return res.send(rows)
+  } catch (error) {
+    console.log(error)
+    return res.status(500).json({
+      message: 'Something goes wrong'
+    })
+  }
+}
+export const deleteReserve = async (req, res) => {
+    try {
+      const { Id} = req.params
+      const [rows] = await pool.query('DELETE FROM coursesdb.cursousuario where cursousuario.Id=?;', [Id])
+      return res.send(rows)
+    } catch (error) {
+      console.log(error)
+      return res.status(500).json({
+        message: 'Something goes wrong'
+      })
+    }
+  }
