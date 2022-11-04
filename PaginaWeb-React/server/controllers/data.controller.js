075760db8116b0764bd84334b9eeebb755fdc306
Franco Balich <franco.balich@gmail.com>
2022-11-03 22:08:29 -0300

    Return first position

--- PaginaWeb-React/server/controllers/data.controller.js
+++ PaginaWeb-React/server/controllers/data.controller.js
@@ -3,7 +3,7 @@ import { pool } from '../db.js'
 export const getDataNumReservas = async (req, res) => {
   try {
     const [rows] = await pool.query('SELECT COUNT(cursousuario.ID) AS NumeroDeReservas FROM coursesdb.cursousuario;')
-    return res.json(rows)
+    return res.json(rows[0])
   } catch (error) {
     console.log(error)
     return res.status(500).json({
@@ -14,7 +14,7 @@ export const getDataNumReservas = async (req, res) => {
 export const getDataNumCursos = async (req, res) => {
   try {
     const [rows] = await pool.query('SELECT COUNT(curso.id) AS NumeroDeCursos FROM coursesdb.curso;')
-    return res.json(rows)
+    return res.json(rows[0])
   } catch (error) {
     console.log(error)
     return res.status(500).json({
@@ -25,7 +25,7 @@ export const getDataNumCursos = async (req, res) => {
 export const getDataNumAlumnos = async (req, res) => {
   try {
     const [rows] = await pool.query('SELECT COUNT(DISTINCT cursousuario.NombreDeUsuario) AS NumeroDeAlumnos FROM coursesdb.cursousuario;')
-    return res.json(rows)
+    return res.json(rows[0])
   } catch (error) {
     console.log(error)
     return res.status(500).json({
@@ -36,7 +36,7 @@ export const getDataNumAlumnos = async (req, res) => {
 export const getDataCursos= async (req, res) => {
   try {
     const [rows] = await pool.query('SELECT curso.Id, curso.Titulo, CONCAT (curso.Categoria,", ", curso.Subcategoria) as Categorias,COUNT(curso.Id)AS NumeroDeReservas FROM coursesdb.cursousuario INNER JOIN coursesdb.curso On cursousuario.CursoID= curso.Id GROUP BY curso.Id ORDER BY NumeroDeReservas DESC; ')
-    return res.json(rows)
+    return res.json(rows[0])
   } catch (error) {
     console.log(error)
     return res.status(500).json({
