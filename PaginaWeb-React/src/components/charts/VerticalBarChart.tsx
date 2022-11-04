0421245c8ccf46c476697c7c7b1035d3b9d1dd72
Franco Balich <franco.balich@gmail.com>
2022-11-04 13:41:27 -0300

    Add charts components

---
+++ PaginaWeb-React/src/components/charts/VerticalBarChart.tsx
@@ -0,0 +1,48 @@
+import {  Chart as ChartJS,  CategoryScale,  LinearScale,  BarElement,  Title, Tooltip, Legend } from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+import { faker } from '@faker-js/faker'
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top' as const,
+    },
+    title: {
+      display: true,
+      text: 'Analiticas de reservas',
+    },
+  },
+};
+
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+const data = {
+  labels,
+  datasets: [
+    {
+      label: 'Dataset 1',
+      data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+      backgroundColor: 'rgba(255, 99, 132, 0.5)',
+    },
+    {
+      label: 'Dataset 2',
+      data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+      backgroundColor: 'rgba(53, 162, 235, 0.5)',
+    },
+  ],
+};
+
+ function VerticalBarChart() {
+  return <Bar options={options} data={data} />;
+}
+export default VerticalBarChart
