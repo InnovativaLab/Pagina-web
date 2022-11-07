2adb7d7c56b5338e032616a87dc37e17489e968b
Franco Balich <franco.balich@gmail.com>
2022-11-07 19:49:58 -0300

    Fix server port

--- PaginaWeb-React/server/config.js
+++ PaginaWeb-React/server/config.js
@@ -1,7 +1,7 @@
 import { config } from 'dotenv'
 config()
 
-export const PORT = process.env.PORT || '3000'
+export const PORT = process.env.PORT || '3001'
 export const DB_HOST = process.env.DB_HOST || 'localhost'
 export const DB_PORT = process.env.DB_PORT || '3306'
 export const DB_USER = process.env.DB_USER || 'root'
