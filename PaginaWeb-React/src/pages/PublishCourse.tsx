168476bb737bb5b28b289da3b6145eb996753bb3
Franco Balich <franco.balich@gmail.com>
2022-11-07 00:30:33 -0300

    Add auth

--- PaginaWeb-React/src/pages/PublishCourse.tsx
+++ PaginaWeb-React/src/pages/PublishCourse.tsx
@@ -1,6 +1,39 @@
-import React from 'react'
+import { getDataNumReservas, getDataNumCursos, getDataNumAlumnos, getDataCursos, getDataAnalisis } from '../services/services'
+import VerticalBarChart from '../components/charts/VerticalBarChart'
+import svgSombreroEgresado from '../assets/SombreroEgresado.svg'
+import LineChart from '../components/charts/LineChart'
+import PieChart from '../components/charts/PieChart'
+import { userSesion } from '../services/userSesion'
+import svgEstrella from '../assets/Estrella.svg'
+import svgReservas from '../assets/Reservas.svg'
+import { useNavigate } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import svgUser from '../assets/User.svg'
+import { enumPermisos } from '../enum'
+import './styles/Teacher.css'
+import { DataAnalisis } from '../types'
 
 function PublishCourse() {
+  const sesion = userSesion.getInstance()
+  const [numReservas, setNumReservas] = useState(0)
+  const [numCourses, setNumCourses] = useState(0)
+  const [numStudents, setNumStudents] = useState(0)
+  const [dataCourses, setDataCourses] = useState(0)
+  const [dataAnalisis, setDataAnalisis] = useState(0)
+  const [fechas, setFechas] = useState(<></>)
+  const navigate = useNavigate()
+  window.scrollTo(0, 0)
+  
+  useEffect(() => {
+    if (!sesion.isAuthorized(enumPermisos.VerAnaliticas)) {
+      navigate('/', { replace: true })
+    } else {
+      const userDataSesion = sesion.readSesion()
+      if (userDataSesion?.RolNombre !== 'Docente') {
+        navigate('/home', { replace: true })
+      }
+    }
+  }, [])
   return (
     <div>PublicCuorse</div>
   )
