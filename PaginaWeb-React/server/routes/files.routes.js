fc0f23fc95c7ba012a81b7ca3fbc4a792eea28be
Franco Balich <franco.balich@gmail.com>
2022-11-10 16:09:10 -0300

    Fix format

--- PaginaWeb-React/server/routes/files.routes.js
+++ PaginaWeb-React/server/routes/files.routes.js
@@ -3,12 +3,12 @@ import multer from 'multer'
 import mimeTypes from 'mime-types'
 
 const router = Router()
-let fileName=""
+let fileName = ''
 const storage = multer.diskStorage({
   destination: './server/uploads/',
   filename: (req, file, cb) => {
-    fileName=`${Date.now()}.${mimeTypes.extension(file.mimetype)}`
-    cb('',fileName)
+    fileName = `${Date.now()}.${mimeTypes.extension(file.mimetype)}`
+    cb('', fileName)
   }
 })
 
@@ -16,17 +16,16 @@ const upload = multer({
   storage
 })
 router.post('/saveFile', upload.single('profileImg'), async (req, res) => {
-    try {
-      console.log('Se guardo el archivo')
-      res.json({
-          message: fileName
-        })
-    } catch (error) {
-      console.log(error)
-      return res.status(500).json({
-        message: 'Something goes wrong'
-      })
-    }
-  })
+  try {
+    res.json({
+      message: fileName
+    })
+  } catch (error) {
+    console.log(error)
+    return res.status(500).json({
+      message: 'Something goes wrong'
+    })
+  }
+})
 
 export default router
