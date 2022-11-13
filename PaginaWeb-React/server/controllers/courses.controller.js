1b2568281bb945e3b53181051d0fdd865d15ec9d
Franco Balich <franco.balich@gmail.com>
2022-11-13 15:05:02 -0300

    Add control state of course

--- PaginaWeb-React/server/controllers/courses.controller.js
+++ PaginaWeb-React/server/controllers/courses.controller.js
@@ -5,11 +5,11 @@ export const getCourses = async (req, res) => {
     const { Titulo, Cantidad } = req.body
     let query = ''
     if (Titulo === undefined && Cantidad === undefined) {
-      query = 'SELECT * from Curso;'
+      query = 'SELECT * from Curso WHERE curso.Estado="Aprobado";'
     } else if (Titulo !== undefined && Cantidad === undefined) {
-      query = `SELECT * from Curso Where Titulo Like "%${Titulo}%";`
+      query = `SELECT * from Curso Where Titulo Like "%${Titulo}%" AND Estado="Aprobado";`
     } else {
-      query = `SELECT * FROM coursesdb.Curso LIMIT ${Cantidad};`
+      query = `SELECT * FROM coursesdb.Curso WHERE Estado="Aprobado" LIMIT ${Cantidad} ;`
     }
     const [rows] = await pool.query(query)
     res.json(rows)
@@ -22,7 +22,7 @@ export const getCourses = async (req, res) => {
 }
 export const getCourse = async (req, res) => {
   try {
-    const [rows] = await pool.query('SELECT * from Curso WHERE id=?;', [req.params.id])
+    const [rows] = await pool.query('SELECT * from Curso WHERE id=? and curso.Estado="Aprobado";', [req.params.id])
     if (rows.length <= 0) return res.status(404).json({ message: 'Course not found' })
     res.json(rows[0])
   } catch (error) {
