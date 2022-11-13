c5b0a13c22e3f9290eb534f379d3df6c40a2e4f4
Franco Balich <franco.balich@gmail.com>
2022-11-13 16:46:05 -0300

    Add Patch method

--- PaginaWeb-React/server/index.js
+++ PaginaWeb-React/server/index.js
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*')
   // res.header('Accept-Encoding: gzip, compress, br')
   res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method')
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE')
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE, PATCH')
   res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE')
   next()
 })
