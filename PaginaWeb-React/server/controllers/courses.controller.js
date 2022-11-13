c9d204090956c95e9fcf95d3b1592583c5a24c4b
Franco Balich <franco.balich@gmail.com>
2022-11-13 18:54:05 -0300

    Add get to remove courses

--- PaginaWeb-React/server/controllers/courses.controller.js
+++ PaginaWeb-React/server/controllers/courses.controller.js
@@ -23,7 +23,7 @@ export const getCourses = async (req, res) => {
 
 export const getCourse = async (req, res) => {
   try {
-    const [rows] = await pool.query('SELECT * from Curso WHERE id=? and curso.Estado="Aprobado";', [req.params.id])
+    const [rows] = await pool.query('SELECT * from Curso WHERE id=?;', [req.params.id])
     if (rows.length <= 0) return res.status(404).json({ message: 'Course not found' })
     res.json(rows[0])
   } catch (error) {
